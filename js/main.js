117f8e25d2b6aad0473bde102d470c611417f6d6
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2018-06-25 19:49:53 -0400

    highlight active nav link

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 window.addEventListener('load', () => {
-  console.log("ready.");
+  console.log('ready.');
+
+  // mark the active nav link
+  let currentURL = location.href;
+  document.querySelector('nav').querySelectorAll('a').forEach( (element) => {
+    let linkURL = element.href;
+    if (currentURL === linkURL) {
+      element.classList.add('active');
+    }
+  })
+
+
 }, false);
